d041c841ff910fccfdb67fac7b3213f5d8b58d80
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2022-09-09 11:02:33 +0200

    changes made in model

--- models/Itinerary.model.js
+++ models/Itinerary.model.js
@@ -1,15 +1,15 @@
 const { Schema, model } = require("mongoose");
 
 const itinerarySchema = new Schema({
-  title: {type: String, required: true},
-  imageUrl: {type: String},
-  countries: [String],
-  cities: [String],
-  flightDetails: [Object],
-  hotelDetails: [Object],
-  activities: [Object],
-  notes: [String],
-  isPublic: {type: Boolean, default: false},
+  title: { type: String, required: true },
+  imageUrl: { type: String },
+  countries: { type: [String] },
+  cities: { type: [String] },
+  flightDetails: { type: [Object] },
+  hotelDetails: { type: [Object] },
+  activities: { type: [Object] },
+  notes: { type: [String] },
+  isPublic: { type: Boolean, default: false },
   user: { type: Schema.Types.ObjectId, ref: "User" }
 })
 
--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   imageUrl: { type: String },
   itineraries: [{ type: Schema.Types.ObjectId, ref: "Itinerary" }], 
 })
