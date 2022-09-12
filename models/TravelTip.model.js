e15eed36a935a6e86706201601dead0c060d8178
Sunairaa <sunairashahzeb@gmail.com>
2022-09-12 19:07:55 +0200

    added category field in model

--- models/TravelTip.model.js
+++ models/TravelTip.model.js
@@ -3,7 +3,12 @@ const { Schema, model } = require("mongoose");
 const travelTipSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  category: { type: String, required: true },
+  category: { 
+    type: String, 
+    enum: ["Travel", "Flight", "Hotel", "Other"],
+    default: "Travel",
+    required: true
+   },
   owner: { type: Schema.Types.ObjectId, ref: "User" }
 })
 
