bffb8057e188fe20a7cb7a891e946ed8a453312e
Sunairaa <sunairashahzeb@gmail.com>
2022-09-12 17:16:10 +0200

    did correction

--- models/Tip.model.js
+++ models/Tip.model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const tipSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User" }
+  owner: { type: Schema.Types.ObjectId, ref: "User" }
 })
 
 const Tip = model("Itinerary", tipSchema);
